d44ee36cbcfe61f4df04b9a7428fab2b050e7db2
komolafetemitope14 <59484726+komolafetemitope14@users.noreply.github.com>
2024-10-31 09:55:18 +0100

    Update fortest.js

--- fortest.js
+++ fortest.js
@@ -8,5 +8,8 @@
 
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
 
+<p id="styleDemo">JavaScript can also change styles!</p>
+<button type="button" onclick='document.getElementById("styleDemo").style.color = "blue"'>Change Text Color</button>
+
 </body>
 </html>
