a03dedbec790bcf4264f6a18b51a63ebc20f330a
komolafetemitope14 <59484726+komolafetemitope14@users.noreply.github.com>
2024-11-05 13:54:45 +0100

    Update fortest.js

--- fortest.js
+++ fortest.js
@@ -11,5 +11,8 @@
 <p id="styleDemo">JavaScript can also change styles!</p>
 <button type="button" onclick='document.getElementById("styleDemo").style.color = "blue"'>Change Text Color</button>
 
+<p id="multiChange">JavaScript can change multiple properties at once!</p>
+<button type="button" onclick='document.getElementById("multiChange").style.color = "green"; document.getElementById("multiChange").style.fontSize = "20px"; document.getElementById("multiChange").innerHTML = "JavaScript is powerful!"'>Change Text and Style</button>
+
 </body>
 </html>
