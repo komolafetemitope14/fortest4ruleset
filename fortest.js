95294c102813df48d34708b076dc7d91c68a4e20
komolafetemitope14 <komolafetemitope14@gmail.com>
2024-11-12 09:09:23 +0100

    Added a button to hide the text in styleDemo paragraph

---
+++ fortest.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+<p id="styleDemo">JavaScript can also change styles!</p>
+<button type="button" onclick='document.getElementById("styleDemo").style.color = "blue"'>Change Text Color</button>
+
+<!-- New functionality to hide the paragraph -->
+<button type="button" onclick='document.getElementById("styleDemo").style.display = "none"'>Hide Text</button>
+
+</body>
+</html>
